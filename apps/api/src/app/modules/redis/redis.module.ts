0246221e163633aeb1e928851763855742c742b2
Adriana Olszak <adriana.olszak@gmail.com>
2020-10-04 11:10:27 +0200

    Set up connection with redis

---
+++ apps/api/src/app/modules/redis/redis.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+
+import { redisProviders } from './redis.providers';
+import { RedisService } from './redis.service';
+
+@Module({
+  providers: [...redisProviders, RedisService],
+  exports: [...redisProviders, RedisService],
+})
+export class RedisModule {
+}
