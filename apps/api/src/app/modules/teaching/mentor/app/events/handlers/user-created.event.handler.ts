197193ab8ca0f2788dfdc377f21f225e5fac1b26
Adriana Olszak <adriana.olszak@gmail.com>
2020-10-12 23:30:49 +0200

    create student or mentor

--- apps/api/src/app/modules/teaching/mentor/app/events/handlers/user-created.event.handler.ts
+++ apps/api/src/app/modules/teaching/mentor/app/events/handlers/user-created.event.handler.ts
@@ -2,16 +2,16 @@ import { EventsHandler, IEventHandler } from '@nestjs/cqrs';
 import UserCreatedEvent from 'apps/api/src/app/modules/user/app/events/implements/user-created.event';
 import { Injectable } from '@nestjs/common';
 import { CreateMentorUseCase } from '../../use-cases/create-mentor/create-mentor.use-case';
+import { USER_ACCESS } from 'apps/api/src/app/modules/user/domain/model/user';
 
 @Injectable()
 @EventsHandler(UserCreatedEvent)
 export class UserCreatedEventHandler
   implements IEventHandler<UserCreatedEvent> {
-
   constructor(private readonly useCase: CreateMentorUseCase) {}
 
-  async handle({id}: UserCreatedEvent) {
-    // just an example
-    await this.useCase.execute({userId: id})
+  async handle({ id, access }: UserCreatedEvent) {
+    if (access === USER_ACCESS.MENTOR)
+      await this.useCase.execute({ userId: id });
   }
 }
