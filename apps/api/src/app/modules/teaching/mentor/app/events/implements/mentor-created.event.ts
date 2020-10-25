33d14939f23fe0768ab114551b446419d71d0ff0
Adriana Olszak <adriana.olszak@gmail.com>
2020-10-25 20:57:07 +0100

    fix event params

--- apps/api/src/app/modules/teaching/mentor/app/events/implements/mentor-created.event.ts
+++ apps/api/src/app/modules/teaching/mentor/app/events/implements/mentor-created.event.ts
@@ -1,5 +1,5 @@
 import { IEvent } from '@nestjs/cqrs';
 
 export default class MentorCreatedEvent implements IEvent {
-  constructor(public readonly id: string, public readonly price: number, public readonly timeSlotsCount) {}
+  constructor(public readonly id: string) {}
 }
